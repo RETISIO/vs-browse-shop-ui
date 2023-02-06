27613ea08af379e8c22332ddc18a9fa974fe2af9
Harish <kjharish@aienterprise.com>
2023-02-06 19:08:35 +0530

    layout changes

--- shared/context/pageData-context.js
+++ shared/context/pageData-context.js
@@ -3,15 +3,16 @@ import { createContext, useContext, useState } from 'react';
 const PageDataContext = createContext();
 
 export function PageDataContextWrapper({ children }) {
-    const [pageData, setPageData] = useState(null);
+  const [pageData, setPageData] = useState(null);
 
-    return (
-        <PageDataContext.Provider value={{ pageData, setPageData }}>
-            {children}
-        </PageDataContext.Provider>
-    );
+  return (
+    // eslint-disable-next-line react/jsx-no-constructed-context-values
+    <PageDataContext.Provider value={{ pageData, setPageData }}>
+      {children}
+    </PageDataContext.Provider>
+  );
 }
 
 export function usePageDataContext() {
-    return useContext(PageDataContext);
+  return useContext(PageDataContext);
 }
