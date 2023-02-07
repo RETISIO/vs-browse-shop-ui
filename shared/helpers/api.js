a4f7f91709ddae18f4944630c10021e0270a8e5f
Harish <kjharish@aienterprise.com>
2023-02-07 13:44:56 +0530

    AB-426

--- shared/helpers/api.js
+++ shared/helpers/api.js
@@ -1,12 +1,13 @@
+/* eslint-disable import/no-import-module-exports */
 import { Service } from '@retisio/sf-api';
 
 const requestContructor = async(URL, params = '', customHeaders = {}, req = null) => {
   let finalHeaders = {}; let tempURI;
   finalHeaders = {
     ...customHeaders,
-    originurl: 'http://us.na.retisio.com',
+    originurl: process.env.NEXT_PUBLIC_CHANNELDOMAIN || 'http://us.ab-dev.retisio.com',
   };
-  finalHeaders['x-chnl-id'] = process.env.NEXT_PUBLIC_CHANNELID;
+  // finalHeaders['x-chnl-id'] = process.env.NEXT_PUBLIC_CHANNELID;
 
   if(req) {
     tempURI = process.env.NEXT_PUBLIC_APIURL + URL + params;
