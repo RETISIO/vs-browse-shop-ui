2be8c02ef54f8177ebde5465f91f21af29f0cdb3
Harish <kjharish@aienterprise.com>
2023-02-07 17:24:28 +0530

    changes

--- pages/plp/[...id].js
+++ pages/plp/[...id].js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-expressions */
-import { useEffect} from 'react';
+import { useEffect } from 'react';
 // eslint-disable-next-line import/named
 import { requestContructor } from '../../shared/helpers/api';
 import { usePageDataContext } from '../../shared/context/pageData-context';
@@ -7,7 +7,7 @@ import PageBuilder from '../../shared/components/layout/pageBuilder';
 import MainLayout from '../layout';
 
 function Static({ data }) {
-  console.log("ress", data);
+  console.log('ress', data);
   // const i18n = useI18n();
   const { setPageData } = usePageDataContext();
   const pageContent = data?.payLoad?.page?.segmentsMap;
@@ -15,8 +15,8 @@ function Static({ data }) {
     data && setPageData(data);
   }, []);
   return (
-    <MainLayout data={data?.payLoad?.page}>
-      <main>
+    <MainLayout data={data?.payLoad}>
+      <main style={{ minHeight: '300px' }}>
         {/* {i18n.t('title')} */}
         <PageBuilder pageContent={pageContent} />
       </main>
@@ -28,8 +28,8 @@ Static.getInitialProps = async(context) => {
   const { query } = context;
   let res;
   try {
-    res = await requestContructor(`getProductsList`, '', {}, true);
-    console.log("ress", res);
+    res = await requestContructor('getProductsList', '', {}, true);
+    console.log('ress', res);
   } catch (e) {
     res = {};
   }
