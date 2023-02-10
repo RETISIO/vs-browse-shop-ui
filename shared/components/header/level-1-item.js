970cd3d0d9f9f978e7676ce782721c52929961ab
dattaprasad <dbhangle@aienterprise.com>
2023-02-10 11:34:59 +0530

    Removed the unwanted code related to HomePage

--- shared/components/header/level-1-item.js
+++ shared/components/header/level-1-item.js
@@ -3,10 +3,7 @@ import LevelTwoItem from './level-2-item';
 
 export default function LevelOneItem(props) {
   const { headerType, rootCatagories } = props;
-  const navContainerClass = headerType === 'nonHome' ? 'container' : '';
-  let headerDropdownMenuClass = 'header-dropdown-menu dropdown-menu js-dropdown__body';
-  headerDropdownMenuClass = headerType === 'home'
-    ? `${headerDropdownMenuClass} homeDropdownMenu` : headerDropdownMenuClass;
+  const headerDropdownMenuClass = 'header-dropdown-menu dropdown-menu js-dropdown__body homeDropdownMenu';
 
   const toggleHeaderDropdownMenu = (event, displayHeaderMenu) => {
     const navEl = event.target.closest('.navListItems').querySelector('.header-dropdown-menu.dropdown-menu');
@@ -33,36 +30,18 @@ export default function LevelOneItem(props) {
         onMouseEnter={(e) => mouseOverOnNav(e)}
         onMouseLeave={(e) => mouseOutOnNav(e)}
       >
-        {headerType === 'nonHome'
-          ? <a className="js-dropdown__btn" href="/category/shop-beef" id={`header-nav-${navItem}`}>{navItem}</a>
-          : (
-            <button className="js-dropdown__btn" aria-expanded="false" id="header-nav-beef">
-              {navItem}
-            </button>
-          )}
+        <button className="js-dropdown__btn" type="button" aria-expanded="false" id="header-nav-beef">
+          {navItem}
+        </button>
         <div className={headerDropdownMenuClass} id="header-navbar">
-          {headerType === 'home'
-            ? (
-              <button
-                onClick={() => navElementsCloseBtnHandler()}
-                className="close-button"
-                type="button"
-                style={{
-                  position: 'absolute',
-                  top: '1px',
-                  right: '19px',
-                  padding: '2px 17px',
-                  background: 'none',
-                  border: 0,
-                  fontSize: '18px',
-                  cursor: 'pointer',
-                }}
-              >
-                <span className="fas fa-times" aria-hidden="true"></span>
-                <span className="sr-only">Close menu</span>
-              </button>
-            )
-            : null}
+          <button
+            onClick={() => navElementsCloseBtnHandler()}
+            className="close-button"
+            type="button"
+          >
+            <span className="fas fa-times" aria-hidden="true"></span>
+            <span className="sr-only">Close menu</span>
+          </button>
           <LevelTwoItem
             catItem={item}
             headerType={headerType}
@@ -73,7 +52,7 @@ export default function LevelOneItem(props) {
   };
   return (
     <nav className="navbar">
-      <div className={navContainerClass}>
+      <div>
         <ul className="navbar-nav nav">
           {rootCatagories?.map((item, i) => renderLevel1Item(item, i))}
         </ul>
