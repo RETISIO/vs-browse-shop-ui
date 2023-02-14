979b9d2afbde4fbab5632fb498e7f7456d8abd43
aholla-aie <aholla@aienterprise.com>
2023-02-14 14:07:06 +0530

    AB-47

--- pages/plp/template/resultList.js
+++ pages/plp/template/resultList.js
@@ -11,6 +11,50 @@ export function ResultList() {
         return (
           <div className="col-md-4 col-sm-6 col-xs-12">
             <div className="product-card" data-mh="product-card">
+              {!value?.productPrice?.onSale ? (
+                <div className="product-badge sale">
+                  <div id="cc_img__resize_wrapper-sale-badge" className="">
+                    <img
+                      alt="sale"
+                      data-error-src="/file/v1375487262462743897/general/missing-image.jpg"
+                      data-default-error-src="/img/no-image.jpg"
+                      className="ccLazyLoaded"
+                      data-lazy-loading-image-className="ccLazyLoad"
+                      data-lazy-loaded-image-className="ccLazyLoaded"
+                      data-lazy-loading-parent-className="ccLazyLoad-background"
+                      data-src="/file/general/sale-badge.png"
+                      data-lazy-loading="false"
+                      src="https://www.allenbrothers.com/file/general/sale-badge.png"
+                      data-srcset="/ccstore/v1/images/?source=/file/general/sale-badge.png&amp;height=50&amp;width=50 50w"
+                      data-sizes="(max-width:480px) 50px,(min-width:481px) and (max-width:768px) 50px,(min-width:769px) and (max-width:979px) 50px,(min-width:980px) 50px"
+                      srcset="https://www.allenbrothers.com/ccstore/v1/images/?source=/file/general/sale-badge.png&amp;height=50&amp;width=50 50w"
+                      sizes="(max-width:480px) 50px,(min-width:481px) and (max-width:768px) 50px,(min-width:769px) and (max-width:979px) 50px,(min-width:980px) 50px"
+                    />
+                  </div>
+                </div>
+              ) : null}
+              {value?.additionalDetails?.isPrimeProduct ? (
+                <div className="product-badge usda">
+                  <div id="cc_img__resize_wrapper-usda-badge" className="">
+                    <img
+                      alt="usda"
+                      data-error-src="/file/v1375487262462743897/general/missing-image.jpg"
+                      data-default-error-src="/img/no-image.jpg"
+                      className="ccLazyLoaded"
+                      data-lazy-loading-image-className="ccLazyLoad"
+                      data-lazy-loaded-image-className="ccLazyLoaded"
+                      data-lazy-loading-parent-className="ccLazyLoad-background"
+                      data-src="/file/general/usda-badge.png"
+                      data-lazy-loading="false"
+                      src="https://www.allenbrothers.com/file/general/usda-badge.png"
+                      data-srcset="/ccstore/v1/images/?source=/file/general/usda-badge.png&amp;height=45&amp;width=55 55w"
+                      data-sizes="(max-width:480px) 55px,(min-width:481px) and (max-width:768px) 55px,(min-width:769px) and (max-width:979px) 55px,(min-width:980px) 55px"
+                      srcset="https://www.allenbrothers.com/ccstore/v1/images/?source=/file/general/usda-badge.png&amp;height=45&amp;width=55 55w"
+                      sizes="(max-width:480px) 55px,(min-width:481px) and (max-width:768px) 55px,(min-width:769px) and (max-width:979px) 55px,(min-width:980px) 55px"
+                    />
+                  </div>
+                </div>
+              ) : null}
               <a
                 className="product-image"
                 href="/products/month-plan-duo/10916"
