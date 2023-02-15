782c5bbd6c77ac30d9aa12ac40ac50a3a89d037d
Harish <kjharish@aienterprise.com>
2023-02-15 15:01:19 +0530

    page changes

--- pages/plp/[...id].js
+++ pages/plp/[...id].js
@@ -2,38 +2,30 @@
 /* eslint-disable no-unused-expressions */
 import { useEffect } from 'react';
 // eslint-disable-next-line import/named
+import { useRouter } from 'next/router';
 import { requestContructor } from '../../shared/helpers/api';
 import { usePageDataContext } from '../../shared/context/pageData-context';
 import PageBuilder from './pageBuilder';
 import MainLayout from '../layout';
-import { useRouter } from 'next/router';
 
 function Static({ data }) {
   // const i18n = useI18n();
-  let navigate = useRouter();
+  const navigate = useRouter();
   const { setPageData } = usePageDataContext();
   const pageContent = data?.payLoad?.page?.segmentsMap;
   data.page = data?.payLoad?.page;
-
-
-  const getData = async () => {
-    const res = await requestContructor(`getProductsList?CategoryId=${navigate.query.id.join("+")}`, '', {}, false);
-    console.log('reshoihoi', res);
-    return res;
-  };
   useEffect(() => {
     data && setPageData(data);
-   let m = getData();
-   console.log("navigate.query",m);
+    // const m = getData();
+    // console.log('navigate.query', m);
   //  (async() => {
   //   let res = await requestContructor(`getProductsList?CategoryId=${navigate.query.id.join("+")}`, '', {}, false);
   //  console.log("ressss",res)
  // })
-
   }, [navigate.query.id]);
   return (
     <MainLayout data={data}>
-      <main >
+      <main>
         {/* {i18n.t('title')} */}
         <PageBuilder pageContent={pageContent} />
       </main>
@@ -42,10 +34,10 @@ function Static({ data }) {
 }
 
 Static.getInitialProps = async(context) => {
-  const { query } = context;
+  const { query, req } = context;
   let res;
   try {
-    res = await requestContructor(`getProductsList?CategoryId=${query.id}`, '', {}, true);
+    res = await requestContructor(`getProductsList?CategoryId=${query.id}`, '', {}, !!req);
   } catch (e) {
     res = {};
   }
