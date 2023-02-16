02b58d4805415ff52871f50a2383a64d705fbae1
aholla-aie <aholla@aienterprise.com>
2023-02-16 16:19:44 +0530

    AB-46

--- shared/template/breadcrumb.js
+++ shared/template/breadcrumb.js
@@ -1,26 +1,31 @@
-import React from "react";
-import { usePageDataContext } from "../context/pageData-context";
+/* eslint-disable linebreak-style */
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
+import { usePageDataContext } from '../context/pageData-context';
 
-export function Breadcrumb() {    
+export function Breadcrumb(props) {
+  const { data } = props;
+  const [pageContentData, setPageContent] = useState(data);
   const { pageData } = usePageDataContext();
+  useEffect(() => {
+    setPageContent(pageData);
+  }, [pageData]);
 
   return (
     <>
       <li>
         <Link href="/">Home</Link>
       </li>
-      {pageData?.payLoad?.navigationPath?.map((item, index) => {
-        return (
-          <li className={!item?.isRootCategory ? "active" : ""}>
-            {pageData?.payLoad?.navigationPath?.length-1 !== index ? (
-              <Link href={`${item.id}?id=${item.id}`}>{item?.name}</Link>
-            ) : (
-              <span>{item?.name}</span>
-            )}
-          </li>
-        );
-      })}
+      {pageContentData?.payLoad?.navigationPath?.map((item, index) => (
+        <li className={!item?.isRootCategory ? 'active' : ''}>
+          {pageContentData?.payLoad?.navigationPath?.length-1 !== index ? (
+            <Link href={`${item.id}?id=${item.id}`}>{item?.name}</Link>
+          ) : (
+            <span>{item?.name}</span>
+          )}
+        </li>
+      ))}
     </>
   );
 }
+export default Breadcrumb;
