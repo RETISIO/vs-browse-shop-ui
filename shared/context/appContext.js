1c98525b4dd976c8e0891ba0f5d637ba714709a3
Harish <kjharish@aienterprise.com>
2023-02-20 15:25:39 +0530

    AB-32

--- shared/context/appContext.js
+++ shared/context/appContext.js
@@ -1,12 +1,23 @@
+/* eslint-disable import/named */
 /* eslint-disable react/jsx-no-constructed-context-values */
-import { createContext, useContext, useState } from 'react';
+import { getCookie } from '@retisio/sf-api';
+import {
+  createContext, useContext, useState, useEffect,
+} from 'react';
 import globalState from './globalstate.json';
+import { requestContructor } from '../helpers/api';
 
 const AppContext = createContext();
 
 export function AppWrapper({ children }) {
-  const [state, updateState] = useState(globalState);
-
+  const [state, updateState] = useState(globalState, '', {}, false);
+  useEffect(() => {
+    (async() => {
+      if(!getCookie('x-anyms-id')) {
+        await requestContructor('getUUID');
+      }
+    })();
+  }, []);
   return (
     <AppContext.Provider value={{ state, updateState }}>
       {children}
