4129dd050bbd8b5d5c86429fccbbeca97ef20ecd
aholla-aie <aholla@aienterprise.com>
2023-03-17 19:16:55 +0530

    AB-37

--- public/js/listrack.js
+++ public/js/listrack.js
@@ -36,34 +36,34 @@
     // const element = document.querySelector('.ltkmodal-email');
     // element.addEventListener('click', () => { alert('in'); });
     
-  window.lstImplement = {
-    emailSignUP: (obj) => {
-      _ltk.Signup.New(obj.formId, obj.inputId, obj.type ? _ltk.Signup.TYPE[obj.type] : _ltk.Signup.TYPE.CLICK, obj.buttonId);
-    
-      if (obj.optinId) {
-        _ltk.Signup.SetOptIn(obj.formId, obj.optinId);
-      }
-    
-      if (obj.formFields) {
-        const fields = Object.getOwnPropertyNames(obj.formFields);
-    
-        for (var i = 0; i < fields.length; i++) {
-          var field = fields[i];
-          _ltk.Signup.SetFieldWithKey(obj.formId, obj.formFields[field], field);
-        }
-      }
-    
-      if (obj.formValues) {
-        const values = Object.getOwnPropertyNames(obj.formValues);
-    
-        for (var i = 0; i < values.length; i++) {
-          var field = values[i];
-          _ltk.Signup.SetValue(obj.formId, obj.formValues[field], field);
-        }
-      }
-    },
-    
-  };
-    
     /** ******** End Custom Code *********** */
 }));
+
+window.lstImplement = {
+  emailSignUP: (obj) => {
+    _ltk.Signup.New(obj.formId, obj.inputId, obj.type ? _ltk.Signup.TYPE[obj.type] : _ltk.Signup.TYPE.CLICK, obj.buttonId);
+  
+    if (obj.optinId) {
+      _ltk.Signup.SetOptIn(obj.formId, obj.optinId);
+    }
+  
+    if (obj.formFields) {
+      const fields = Object.getOwnPropertyNames(obj.formFields);
+  
+      for (var i = 0; i < fields.length; i++) {
+        var field = fields[i];
+        _ltk.Signup.SetFieldWithKey(obj.formId, obj.formFields[field], field);
+      }
+    }
+  
+    if (obj.formValues) {
+      const values = Object.getOwnPropertyNames(obj.formValues);
+  
+      for (var i = 0; i < values.length; i++) {
+        var field = values[i];
+        _ltk.Signup.SetValue(obj.formId, obj.formValues[field], field);
+      }
+    }
+  },
+  
+};
