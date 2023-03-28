7a63c3f4a8b1e4cc7045231a40d01df1a3de8e3e
Hansraj <hmacha@AIEnterprise.com>
2023-03-28 16:56:41 +0530

    AB-809 changes updated in package json file

--- shared/helpers/api.js
+++ shared/helpers/api.js
@@ -1,40 +1,61 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable operator-linebreak */
+/* eslint-disable semi */
+/* eslint-disable comma-dangle */
+/* eslint-disable linebreak-style */
+/* eslint-disable space-before-function-paren */
 /* eslint-disable no-nested-ternary */
 /* eslint-disable max-len */
 /* eslint-disable import/no-import-module-exports */
-import { Service } from '@retisio/sf-api';
+import { Service } from '@retisio/sf-api'
 
-const requestContructor = async(URL, params = '', options = {}, req = null, pathname = '') => {
-  let finalOriginURl = ''; let
-    relativeurl;
-  if(req) {
-    finalOriginURl = req && req.headers.host.indexOf('localhost') < 0 && req.headers['x-forwarded-proto'] ? `${req.headers['x-forwarded-proto']}://${req.headers.host}` : 'https://us.ab-dev.retisio.com';
-    relativeurl = pathname;
-  }else if(window) {
-    finalOriginURl = window && window.location.origin.indexOf('localhost') < 0 ? window.location.origin : 'https://us.ab-dev.retisio.com';
-    relativeurl = window.location.pathname;
+const requestContructor = async (
+  URL,
+  params = '',
+  options = {},
+  req = null,
+  pathname = ''
+) => {
+  let finalOriginURl = ''
+  let relativeurl
+  if (req) {
+    finalOriginURl =
+      req &&
+      req.headers.host.indexOf('localhost') < 0 &&
+      req.headers['x-forwarded-proto']
+        ? `${req.headers['x-forwarded-proto']}://${req.headers.host}`
+        : 'https://us.ab-dev.retisio.com'
+    relativeurl = pathname
+  } else if (window) {
+    finalOriginURl =
+      window && window.location.origin.indexOf('localhost') < 0
+        ? window.location.origin
+        : 'https://us.ab-dev.retisio.com'
+    relativeurl = window.location.pathname
   }
-  let finalHeaders = { originurl: finalOriginURl, relativeurl }; let tempURI;
-  if(req) {
-    finalHeaders.host = req.headers.host;
+  let finalHeaders = { originurl: finalOriginURl, relativeurl }
+  let tempURI
+  if (req) {
+    finalHeaders.host = req.headers.host
   }
-  if(req && req.headers.correlationid) {
-    finalHeaders.correlationid = req.headers.correlationid;
+  if (req && req.headers.correlationid) {
+    finalHeaders.correlationid = req.headers.correlationid
   }
-  if(options.customHeaders) {
+  if (options.customHeaders) {
     finalHeaders = {
       ...finalHeaders,
-      ...options.customHeaders,
-    };
+      ...options.customHeaders
+    }
   }
   // finalHeaders['x-chnl-id'] = process.env.NEXT_PUBLIC_CHANNELID || 'http://us.ab-dev.retisio.com';
 
-  if(req) {
-    tempURI = process.env.NEXT_PUBLIC_APIURL + URL + params;
-  }else{
-    tempURI = `/arc/${URL}${params}`;
+  if (req) {
+    tempURI = process.env.NEXT_PUBLIC_APIURL + URL + params
+  } else {
+    tempURI = `/arc/${URL}${params}`
   }
-  console.log('final request data', options, finalHeaders, tempURI);
-  return Service.request(tempURI, { ...options, header: finalHeaders });
-};
+  // console.log('final request data', options, finalHeaders, tempURI);
+  return Service.request(tempURI, { ...options, header: finalHeaders })
+}
 
-module.exports = { requestContructor };
+module.exports = { requestContructor }
