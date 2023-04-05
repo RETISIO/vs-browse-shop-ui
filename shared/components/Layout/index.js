f0c3d9f2debec24a1236017134f72c540da68b04
Harish <kjharish@aienterprise.com>
2023-04-05 13:33:26 +0530

    AB-816

--- shared/components/Layout/index.js
+++ shared/components/Layout/index.js
@@ -2,130 +2,130 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable import/named */
 /* eslint-disable no-unused-vars */
-import React, { useState, useEffect } from 'react'
-import Head from 'next/head'
-import { useI18n } from 'next-localization'
-import { useRouter } from 'next/router'
-import { getCookie } from '@retisio/sf-api'
-import { Layout } from '@retisio/sf-ui'
+import React, { useState, useEffect } from 'react';
+import Head from 'next/head';
+import { useI18n } from 'next-localization';
+import { useRouter } from 'next/router';
+import { getCookie } from '@retisio/sf-api';
+import { Layout } from '@retisio/sf-ui';
 // import Login from '../shared/components/Login';
 // eslint-disable-next-line import/named
-import { useAppContext } from '../../context/appContext'
-import { requestContructor } from '../../helpers/api'
-import ComponentMap from '../componentMap'
-import { useMiniCartDataContext } from '../../context/miniCartcontext'
-import getPersonalization from '../../helpers/utils'
+import { useAppContext } from '../../context/appContext';
+import { requestContructor } from '../../helpers/api';
+import ComponentMap from '../componentMap';
+import { useMiniCartDataContext } from '../../context/miniCartcontext';
+import getPersonalization from '../../helpers/utils';
 
-export default function MainLayout ({ data, children }) {
-  const { state, updateState } = useAppContext()
-  const { show, setShow } = useAppContext()
-  const { isLogged } = useAppContext()
-  const router = useRouter()
+export default function MainLayout({ data, children }) {
+  const { state, updateState } = useAppContext();
+  const { show, setShow } = useAppContext();
+  const { isLogged } = useAppContext();
+  const router = useRouter();
 
-  const i18n = useI18n()
-  const [rootCatagories, setRootCatagories] = useState([])
-  const getData = async () => {
-    const res = await requestContructor('getCategoryList', '', {}, false)
+  const i18n = useI18n();
+  const [rootCatagories, setRootCatagories] = useState([]);
+  const getData = async() => {
+    const res = await requestContructor('getCategoryList', '', {}, false);
 
-    setRootCatagories(res?.payLoad?.categories)
-    return res?.payLoad?.categories
-  }
+    setRootCatagories(res?.payLoad?.categories);
+    return res?.payLoad?.categories;
+  };
 
   useEffect(() => {
     if (!state.channelData) {
-      ;(async () => {
+      (async() => {
         let channelData = await requestContructor(
           'getChannelDetails',
           '',
           {},
-          false
-        )
-        channelData = channelData[process.env.NEXT_PUBLIC_CHANNELDOMAIN]
-        updateState({ ...state, channelData })
-        await getPersonalization()
-      })()
+          false,
+        );
+        channelData = channelData[process.env.NEXT_PUBLIC_CHANNELDOMAIN];
+        updateState({ ...state, channelData });
+        await getPersonalization();
+      })();
     }
-  }, [])
+  }, []);
   useEffect(() => {
-    getData()
-  }, [])
+    getData();
+  }, []);
   useEffect(() => {
-    async function changeLocale () {
+    async function changeLocale() {
       if (router.locale === 'en-US') {
-        i18n.set('en-US', await import('../../../locales/en.json'))
-        i18n.locale('en-US')
+        i18n.set('en-US', await import('../../../locales/en.json'));
+        i18n.locale('en-US');
       } else if (router.locale === 'de') {
-        i18n.set('de', await import('../../../locales/de.json'))
-        i18n.locale('de')
+        i18n.set('de', await import('../../../locales/de.json'));
+        i18n.locale('de');
       }
     }
-    changeLocale()
-  }, [router.locale])
+    changeLocale();
+  }, [router.locale]);
   /* *************Mini Cart functionality starts here ***************** */
-  const { miniCartDetails, setMiniCartDetails } = useMiniCartDataContext()
-  const getMiniCartData = async () => {
-    const cartData = await requestContructor('getCartArc', '', {})
+  const { miniCartDetails, setMiniCartDetails } = useMiniCartDataContext();
+  const getMiniCartData = async() => {
+    const cartData = await requestContructor('getCartArc', '', {});
     if (miniCartDetails.itemAdded === true) {
       setMiniCartDetails({
         ...miniCartDetails,
         itemAdded: false,
         miniCartData: cartData,
-        showMiniCart: true
-      })
-      document.querySelector('.ab-miniCart-Icon>i')?.focus()
+        showMiniCart: true,
+      });
+      document.querySelector('.ab-miniCart-Icon>i')?.focus();
     } else {
       setMiniCartDetails({
         ...miniCartDetails,
         itemAdded: false,
-        miniCartData: cartData
-      })
+        miniCartData: cartData,
+      });
     }
-  }
+  };
   useEffect(() => {
     // if ((isLogged || getCookie('arcCartId')) && !('items' in miniCartDetails.miniCartData)
     // && (miniCartDetails.itemAdded === false)) {
     // to get cart details on load of the module.
-    getMiniCartData()
+    getMiniCartData();
     // }
     if (miniCartDetails?.showMiniCart) {
-      setMiniCartDetails({ ...miniCartDetails, showMiniCart: false })
+      setMiniCartDetails({ ...miniCartDetails, showMiniCart: false });
     }
-  }, [])
+  }, []);
   /** ********************* The below useEffect is for triggering the cart api
    after adding the new item in Cart from PDP page ************************ */
   useEffect(() => {
     if (miniCartDetails.itemAdded === true) {
-      getMiniCartData()
+      getMiniCartData();
     }
-  }, [miniCartDetails.itemAdded])
+  }, [miniCartDetails.itemAdded]);
   /* *************Mini Cart functionality ends here *********************** */
-  const [searchAheadData, setSearchAheadData] = useState(null)
-  const getSearchAheadData = async text => {
+  const [searchAheadData, setSearchAheadData] = useState(null);
+  const getSearchAheadData = async(text) => {
     const res = await requestContructor(
       'getTypeAheadArc',
       `?searchKey=${text}&size=4`,
       {},
-      false
-    )
-    setSearchAheadData(res)
-    return res
-  }
-  const searchAheadChangeHandler = text => {
+      false,
+    );
+    setSearchAheadData(res);
+    return res;
+  };
+  const searchAheadChangeHandler = (text) => {
     if (text.trim().length > 2) {
-      getSearchAheadData(text)
+      getSearchAheadData(text);
     } else {
-      setSearchAheadData(null)
+      setSearchAheadData(null);
     }
-  }
-  const damPath = process.env.NEXT_PUBLIC_IMAGEPATH
-  const signout = async () => {
-    const res = await requestContructor('signout', '', {}, false).then(data => {
+  };
+  const damPath = process.env.NEXT_PUBLIC_IMAGEPATH;
+  const signout = async() => {
+    const res = await requestContructor('signout', '', {}, false).then((data) => {
       if (data) {
-        window.location.reload()
+        window.location.reload();
       }
-    })
-    return res
-  }
+    });
+    return res;
+  };
   return (
     <>
       {/* {show ? 'tue' : 'false sdf'}
@@ -138,21 +138,21 @@ export default function MainLayout ({ data, children }) {
           <title>Retisio Page</title>
         )}
         {data && data?.page?.seo?.description ? (
-          <meta name='description' content={data.page.seo.description} />
+          <meta name="description" content={data.page.seo.description} />
         ) : (
-          <meta name='description' content='restisio home' />
+          <meta name="description" content="restisio home" />
         )}
         {data && data?.page?.seo?.keywords ? (
-          <meta name='keywords' content={data.page.seo.keywords} />
+          <meta name="keywords" content={data.page.seo.keywords} />
         ) : (
-          <meta name='keywords' content='' />
+          <meta name="keywords" content="" />
         )}
         {data && data?.page?.seo?.canonicalUrl ? (
-          <link rel='canonical' href={data.page.seo.canonicalUrl} />
+          <link rel="canonical" href={data.page.seo.canonicalUrl} />
         ) : (
-          <link rel='canonical' href='restisio home' />
+          <link rel="canonical" href="restisio home" />
         )}
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Layout
         data={data}
@@ -168,10 +168,11 @@ export default function MainLayout ({ data, children }) {
         isLogged={isLogged}
         signout={() => signout()}
         miniCartDetails={miniCartDetails}
+        isNextJs={true}
         setMiniCartDetails={setMiniCartDetails}
       >
         {children}
       </Layout>
     </>
-  )
+  );
 }
