587aac8f8edd96d67e3be33b16019683a6bf13e8
Hansraj <hmacha@AIEnterprise.com>
2023-04-18 18:45:41 +0530

    AB-479 weights section is implemented

--- shared/components/pdp/pdpdetails.js
+++ shared/components/pdp/pdpdetails.js
@@ -1,40 +1,67 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable arrow-parens */
+/* eslint-disable @next/next/no-html-link-for-pages */
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/jsx-closing-tag-location */
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable comma-dangle */
+/* eslint-disable jsx-quotes */
+/* eslint-disable operator-linebreak */
+/* eslint-disable no-unused-vars */
+/* eslint-disable semi */
+/* eslint-disable space-before-function-paren */
 /* eslint-disable no-unused-expressions */
 /* eslint-disable max-len */
-import React, { useEffect, useState } from 'react';
-import Accordion from 'react-bootstrap/Accordion';
-import { Breadcrumb } from '../template/components/breadcrumb';
-import { NextImage } from '../template/components/nextImage';
+import React, { useEffect, useState } from 'react'
+import Accordion from 'react-bootstrap/Accordion'
+import { Breadcrumb } from '../template/components/breadcrumb'
+import { NextImage } from '../template/components/nextImage'
 // import SkuLineItem from './skuLineItem';
-import GiftCard from '../giftCard';
-import SkuSelection from './skuSelection';
+import GiftCard from '../giftCard'
+import SkuSelection from './skuSelection'
+import ProductSkus from './productSkus'
 
 export default function ProductDescription(props) {
-  const pdpData = props?.pdpData?.payLoad;
-  const productSkus = pdpData?.products[0]?.skus && Object.values(pdpData?.products[0]?.skus);
-  const productType = pdpData?.products[0]?.productType;
-  const [showWidget, setShowWidget] = useState(false);
+  const pdpData = props?.pdpData?.payLoad
+  const productSkus =
+    pdpData?.products[0]?.skus && Object.values(pdpData?.products[0]?.skus)
+  const productType = pdpData?.products[0]?.productType
+  const [showWidget, setShowWidget] = useState(false)
+  const [showSaleWidget, setShowSaleWidget] = useState(false)
+
   if (productType === 'giftcard') {
-    return (
-      <GiftCard {...props} />
-    );
+    return <GiftCard {...props} />
   }
+
   useEffect(() => {
-    setShowWidget(true);
+    setShowWidget(true)
     setTimeout(() => {
-      window.yotpo && window.yotpo.refreshWidgets();
-    }, 10);
-  }, []);
-  const damPath = process.env.NEXT_PUBLIC_IMAGEPATH;
-  const productId = pdpData?.products[0]?.productId;
+      window.yotpo && window.yotpo.refreshWidgets()
+    }, 10)
+  }, [])
+
+  const damPath = process.env.NEXT_PUBLIC_IMAGEPATH
+  const productId = pdpData?.products[0]?.productId
+
+  const handleShowOnSaleBadge = showBadge => {
+    console.log('from handleShowOnSaleBadge...showBadge...', showBadge)
+    setShowSaleWidget(showBadge)
+  }
+
   const renderGalleryImage = () => {
-    const defaultSkuId = pdpData?.products[0]?.defaultSkuId;
-    const mediaObj = pdpData?.products[0]?.skus[defaultSkuId]?.media;
-    const thumbnailHeight = 475;
-    const thumbnailWidth = 475;
+    const defaultSkuId = pdpData?.products[0]?.defaultSkuId
+    const mediaObj = pdpData?.products[0]?.skus[defaultSkuId]?.media
+    const thumbnailHeight = 475
+    const thumbnailWidth = 475
     return (
-      <aside className="col-md-5">
-        <div className="product-gallery app-figure" id="zoom-fig">
-          <div id="cc_img__resize_wrapper-badge-usda-prime" style={{ maxWidth: '100%', minHeight: '0px', height: '100%' }}>
+      <aside className='col-md-5'>
+        <div className='product-gallery app-figure' id='zoom-fig'>
+          <div
+            id='cc_img__resize_wrapper-badge-usda-prime'
+            style={{ maxWidth: '100%', minHeight: '0px', height: '100%' }}
+          >
             {/* <img
               id="usda-logo"
               className="usda-prime-logo"
@@ -49,18 +76,17 @@ export default function ProductDescription(props) {
               src={`${damPath}${mediaObj?.thumbnailImg}`}
               height={thumbnailHeight}
               width={thumbnailWidth}
-              id="usda-logo"
-              className="usda-prime-logo hidden"
+              id='usda-logo'
+              className='usda-prime-logo hidden'
             />
-
           </div>
-          <div className="product-gallery__main">
+          <div className='product-gallery__main'>
             <a
-              className="MagicZoom js-product-gallery-zoom"
-              id="zoom"
-              href="/ccstore/v1/images/?source=/file/v4671930468835559977/products/10032.jpg"
+              className='MagicZoom js-product-gallery-zoom'
+              id='zoom'
+              href='/ccstore/v1/images/?source=/file/v4671930468835559977/products/10032.jpg'
             >
-              <figure className="mz-figure mz-hover-zoom mz-ready">
+              <figure className='mz-figure mz-hover-zoom mz-ready'>
                 {/* <img
                   id="zoom-image"
                   loading="lazy"
@@ -73,69 +99,122 @@ export default function ProductDescription(props) {
                   src={`${damPath}${mediaObj?.thumbnailImg}`}
                   height={thumbnailHeight}
                   width={thumbnailWidth}
-                  id="zoom-image"
+                  id='zoom-image'
                 />
                 <div
-                  className="mz-lens"
+                  className='mz-lens'
                   style={{
-                    top: '0px', transform: 'translate(-10000px, -10000px)', width: '255px', height: '255px',
+                    top: '0px',
+                    transform: 'translate(-10000px, -10000px)',
+                    width: '255px',
+                    height: '255px'
                   }}
                 >
                   <img
-                    src="https://www.allenbrothers.com/ccstore/v1/images/?source=/file/v4671930468835559977/products/10032.jpg&amp;height=475&amp;width=475"
+                    src='https://www.allenbrothers.com/ccstore/v1/images/?source=/file/v4671930468835559977/products/10032.jpg&amp;height=475&amp;width=475'
                     style={{
-                      position: 'absolute', top: '0px', left: '0px', width: '458px', height: '458px', transform: 'translate(-204px, -1px)',
+                      position: 'absolute',
+                      top: '0px',
+                      left: '0px',
+                      width: '458px',
+                      height: '458px',
+                      transform: 'translate(-204px, -1px)'
                     }}
                   />
                 </div>
               </figure>
-
             </a>
           </div>
-          <div className="MagicScroll js-product-gallery-scroll" id="productThumb" data-magic-zoom-id="zoom" data-target-id="pdp-slider-desktop" data-options="orientation: horizontal;" data-bind="mainGallery: mainGallery"></div>
-          <div className="MagicScroll" id="pdp-slider-desktop" data-options="orientation: horizontal;"></div>
+          <div
+            className='MagicScroll js-product-gallery-scroll'
+            id='productThumb'
+            data-magic-zoom-id='zoom'
+            data-target-id='pdp-slider-desktop'
+            data-options='orientation: horizontal;'
+            data-bind='mainGallery: mainGallery'
+          ></div>
+          <div
+            className='MagicScroll'
+            id='pdp-slider-desktop'
+            data-options='orientation: horizontal;'
+          ></div>
         </div>
       </aside>
-    );
-  };
+    )
+  }
+
+  console.log('from pdpDetails...showSaleWidget..', showSaleWidget)
   return (
     <section>
-      <div className="container pdpMainContainer">
-        <nav className="breadcrumbs-block hidden-print">
-          <ol className="breadcrumb">
+      <div className='container pdpMainContainer'>
+        <nav className='breadcrumbs-block hidden-print'>
+          <ol className='breadcrumb'>
             <Breadcrumb data={props.pdpData} />
           </ol>
         </nav>
         <div>
-          <div className="hidden-lg hidden-md visible-sm visible-xs">
-            <div className="alert alert-dismissible hidden-print alert-success undefined" role="alert" style={{ display: 'none' }}>
-              <button className="close" type="button" aria-label="Close"><span aria-hidden="true">×</span></button>
+          <div className='hidden-lg hidden-md visible-sm visible-xs'>
+            <div
+              className='alert alert-dismissible hidden-print alert-success undefined'
+              role='alert'
+              style={{ display: 'none' }}
+            >
+              <button className='close' type='button' aria-label='Close'>
+                <span aria-hidden='true'>×</span>
+              </button>
             </div>
           </div>
-          <div className="alert alert-dismissible hidden-print alert-success undefined" role="alert" style={{ display: 'none' }}>
-            <button data-bind="click: $data.clearSuccessMessages" className="close" type="button" aria-label="Close"><span aria-hidden="true">×</span></button>
+          <div
+            className='alert alert-dismissible hidden-print alert-success undefined'
+            role='alert'
+            style={{ display: 'none' }}
+          >
+            <button
+              data-bind='click: $data.clearSuccessMessages'
+              className='close'
+              type='button'
+              aria-label='Close'
+            >
+              <span aria-hidden='true'>×</span>
+            </button>
           </div>
-          <div className="alert alert-dismissible hidden-print alert-danger undefined" role="alert" style={{ display: 'none' }}>
-            <button data-bind="click: $data.clearErrorMessages" className="close" type="button" aria-label="Close"><span aria-hidden="true">×</span></button>
+          <div
+            className='alert alert-dismissible hidden-print alert-danger undefined'
+            role='alert'
+            style={{ display: 'none' }}
+          >
+            <button
+              data-bind='click: $data.clearErrorMessages'
+              className='close'
+              type='button'
+              aria-label='Close'
+            >
+              <span aria-hidden='true'>×</span>
+            </button>
           </div>
         </div>
-        <div className="product-title-wrapper">
-          <h1 className="page-title">
+        <div className='product-title-wrapper'>
+          <h1 className='page-title'>
             <span>{pdpData?.products[0]?.displayName}</span>
             <div>
               <div
-                htmlFor="reviewsection"
-                className="yotpo bottomLine"
+                htmlFor='reviewsection'
+                className='yotpo bottomLine'
                 data-yotpo-product-id={pdpData?.products[0]?.productId}
-              >
-              </div>
+              ></div>
             </div>
           </h1>
-          <p className="page-short-description">{pdpData?.products[0]?.description}</p>
+          <p className='page-short-description'>
+            {pdpData?.products[0]?.description}
+          </p>
         </div>
-        <div className="row product-gallery-wrapper">
+        <div className='row product-gallery-wrapper'>
           {renderGalleryImage()}
-          <div className="col-md-7">
+          <div className='col-md-7'>
+            <ProductSkus
+              props={props}
+              handleShowOnSaleBadge={handleShowOnSaleBadge}
+            />
             {/* <table className="table table-striped table-responsive-sm product-table">
               <thead>
                 <tr>
@@ -150,43 +229,73 @@ export default function ProductDescription(props) {
                 {productSkus?.map((item, i) => <SkuSelection key={`sku-${i}`} skuItem={item} productId={productId} />)}
               </tbody>
             </table> */}
-            <SkuSelection />
+            {/* <SkuSelection data={props} /> */}
           </div>
         </div>
       </div>
-      
-      <section className="product-info-section ">
-        <div className="container">
-          <Accordion defaultActiveKey="0" flush>
-            <Accordion.Item eventKey="0">
+
+      <section className='product-info-section '>
+        <div className='container'>
+          <Accordion defaultActiveKey='0' flush>
+            <Accordion.Item eventKey='0'>
               <Accordion.Header>
-                <div className="panel panel-default panel-large">
-                  <div className="panel-heading">
-                    <div className="panel-title">
-                      <a id="productInfoSelector" className="collapse" role="button" data-toggle="collapse" href="#productInfoPanel">
+                <div className='panel panel-default panel-large'>
+                  <div className='panel-heading'>
+                    <div className='panel-title'>
+                      <a
+                        id='productInfoSelector'
+                        className='collapse'
+                        role='button'
+                        data-toggle='collapse'
+                        href='#productInfoPanel'
+                      >
                         Product Info
-                        <i className="icon fas fa-chevron-down"></i>
+                        <i className='icon fas fa-chevron-down'></i>
                       </a>
                     </div>
                   </div>
                 </div>
               </Accordion.Header>
               <Accordion.Body>
-                <div className="panel-body" data-bind="html: $data.product().longDescription()">
-                  <p>A fine texture and rich marbling combined with our special dry-aging process give Allen Brothers sirloin strip steaks an incredibly robust flavor and juicy tenderness. Sometimes called a New York strip or shell steak, by any name it's a classic steak-lover's steak</p>
-                  <p>We dry age our steaks in custom-designed, dry aging coolers where a complex process incorporates an intricate and delicate balance of time, temperature, air circulation and humidity. The beef ages openly exposed to the air inside the cooler, from the outside in, forming a hard crust around the edible tissue. After aging is complete, this firm outer crust is carefully removed, leaving meat that is significantly more tender and with the distinctive ‘beefy’ flavor specific to dry aging.</p>
-
+                <div
+                  className='panel-body'
+                  data-bind='html: $data.product().longDescription()'
+                >
+                  <p>
+                    A fine texture and rich marbling combined with our special
+                    dry-aging process give Allen Brothers sirloin strip steaks
+                    an incredibly robust flavor and juicy tenderness. Sometimes
+                    called a New York strip or shell steak, by any name it's a
+                    classic steak-lover's steak
+                  </p>
+                  <p>
+                    We dry age our steaks in custom-designed, dry aging coolers
+                    where a complex process incorporates an intricate and
+                    delicate balance of time, temperature, air circulation and
+                    humidity. The beef ages openly exposed to the air inside the
+                    cooler, from the outside in, forming a hard crust around the
+                    edible tissue. After aging is complete, this firm outer
+                    crust is carefully removed, leaving meat that is
+                    significantly more tender and with the distinctive ‘beefy’
+                    flavor specific to dry aging.
+                  </p>
                 </div>
               </Accordion.Body>
             </Accordion.Item>
-            <Accordion.Item eventKey="1">
+            <Accordion.Item eventKey='1'>
               <Accordion.Header>
-                <div className="panel panel-default panel-large">
-                  <div className="panel-heading">
-                    <div className="panel-title">
-                      <a id="productInfoSelector" className="collapse" role="button" data-toggle="collapse" href="#productInfoPanel">
+                <div className='panel panel-default panel-large'>
+                  <div className='panel-heading'>
+                    <div className='panel-title'>
+                      <a
+                        id='productInfoSelector'
+                        className='collapse'
+                        role='button'
+                        data-toggle='collapse'
+                        href='#productInfoPanel'
+                      >
                         Shipping Info
-                        <i className="icon fas fa-chevron-down"></i>
+                        <i className='icon fas fa-chevron-down'></i>
                       </a>
                     </div>
                   </div>
@@ -194,12 +303,12 @@ export default function ProductDescription(props) {
               </Accordion.Header>
               <Accordion.Body>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum.
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
               </Accordion.Body>
             </Accordion.Item>
           </Accordion>
@@ -230,20 +339,22 @@ export default function ProductDescription(props) {
           </div> */}
         </div>
       </section>
-      <div className="container" id="reviewsection">
+      <div className='container' id='reviewsection'>
         {showWidget && (
           <div
-            className="yotpo yotpo-main-widget"
+            className='yotpo yotpo-main-widget'
             data-product-id={pdpData?.products[0]?.productId}
             data-price={pdpData?.products[0]?.productId}
-            data-currency="USD"
+            data-currency='USD'
             data-name={pdpData?.products[0]?.displayName}
             data-url={window.location.href}
-            data-image-url={`${damPath}${pdpData?.products[0]?.skus[pdpData?.products[0]?.defaultSkuId]?.media?.thumbnailImg}`}
-          >
-          </div>
+            data-image-url={`${damPath}${
+              pdpData?.products[0]?.skus[pdpData?.products[0]?.defaultSkuId]
+                ?.media?.thumbnailImg
+            }`}
+          ></div>
         )}
       </div>
     </section>
-  );
+  )
 }
---
+++ shared/components/pdp/skusBasedOnWeights.js
@@ -0,0 +1,133 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unreachable-loop */
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-const-assign */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable operator-linebreak */
+/* eslint-disable arrow-parens */
+/* eslint-disable array-callback-return */
+/* eslint-disable comma-dangle */
+/* eslint-disable no-unused-vars */
+/* eslint-disable semi */
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable react/button-has-type */
+/* eslint-disable max-len */
+/* eslint-disable space-before-function-paren */
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-quotes */
+import React, { useState, useEffect } from 'react'
+import SkuDetailsOfSelectedWeight from './skuDetailsOfSelectedWeight'
+
+// skuObj = {
+// id:''
+//   wieght: "10oz",
+//   thickness: 'ssss',
+//   count: [
+//     {
+//     pieces: "2pcs",
+//     availableStock: 200,
+//     quantityAddedToCart: 0,
+//      inventoryStatusLabel: '....',
+//     image:xxxx,
+//     thumbnailImgs: [aaa,bbb,ccc],
+//     itemCode: 1111,
+//     salePrice: 1111,
+//     listPrice: 9999,
+// hasPrice: false,
+//   hasStock: false,
+//     onSale: false,
+//     outOfStock: false,
+//     },
+//    ],
+//    addToCart: ()=>{},
+//    addToWishList: ()=>{},
+//    notifyMe: ()=>{}
+//   },
+
+function SkusBasedOnWeights({
+  skusObj,
+  skusWeights,
+  skuWeightSelected,
+  setSkuWeightSelected,
+  defaultSkuId,
+  handleShowOnSaleBadge //   handleSkuSelected
+}) {
+  const [skuSelected, setSkuSelected] = useState()
+
+  useEffect(() => {
+    if (skusObj) {
+      //   const firstKey = defaultSkuId ? '' : Object.keys(skusObj)[0]
+      //   const skuObj = skusObj[firstKey]
+      //   setSkuSelected(skuObj)
+      selectDefault()
+    }
+  }, [skusObj])
+
+  const selectDefault = () => {
+    let skuObj = ''
+    for (const key in skusObj) {
+      if (skusObj[key].id === defaultSkuId) {
+        skuObj = skusObj[key]
+        break
+      } else {
+        const firstKey = Object.keys(skusObj)[0]
+        skuObj = skusObj[firstKey]
+        break
+      }
+    }
+    setSkuSelected(skuObj)
+  }
+
+  const handleSkuSelected = skuObj => {
+    setSkuSelected(skuObj)
+  }
+
+  const formatThickness = str => {
+    if (str[0] === '\\' && str[1] === '|') {
+      return str.slice(3)
+    }
+    return str
+  }
+
+  const displaySkuDetails = () => (
+    <>
+      <div className='sukhead'> Weight: </div>
+      <div>
+        <ul className='list-inline'>
+          {skusObj &&
+            Object.keys(skusObj).map(key => (
+              <li className='list-inline-item me-2 mb-2'>
+                <div
+                  className={
+                    skuSelected &&
+                    skuSelected.weight === skusObj[key].weight &&
+                    skuSelected.thickness === skusObj[key].thickness
+                      ? 'tag-selected'
+                      : 'tag'
+                  }
+                  onClick={() => handleSkuSelected(skusObj[key])}
+                >
+                  <span className='txttagb'>{skusObj[key].weight}</span>
+                  <span className='txttagz'>
+                    {formatThickness(skusObj[key].thickness)}
+                  </span>
+                </div>
+              </li>
+            ))}
+        </ul>
+      </div>
+      {skuSelected && (
+        <SkuDetailsOfSelectedWeight
+          selectedSku={skuSelected}
+          handleShowOnSaleBadge={handleShowOnSaleBadge}
+        />
+      )}
+    </>
+  )
+  return <div className='sukproduct'>{displaySkuDetails()}</div>
+}
+
+export default SkusBasedOnWeights
