b0982ba4818ab12a68d569782decd63da9fc412a
Harish <kjharish@aienterprise.com>
2023-04-19 14:48:11 +0530

    resolve conflict

--- pages/_document.js
+++ pages/_document.js
@@ -4,25 +4,28 @@ import {
 } from 'next/document';
 
 export default function Document() {
+  const GtagSrc = `https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GA_KEY}&gtm_auth=FX9Yprad5blniYKnO31hiw&gtm_preview=env-638&gtm_cookies_win=x`;
+
   return (
-    <><Html lang="en">
+    <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://staticw2.yotpo.com" />
         <link rel="preconnect" href="//www.google-analytics.com" />
         <noscript>
           <iframe
-            src />`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GA_KEY}&gtm_auth=FX9Yprad5blniYKnO31hiw&gtm_preview=env-638&gtm_cookies_win=x`
-          height="0"
-          width="0"
-          style="display:none;visibility:hidden"
+            src={GtagSrc}
+            height="0"
+            width="0"
+            style={{display:"none",visibility:"hidden"}}
           >
-        </iframe>
+          </iframe>
 
-      </noscript>
-    </Head><body>
+        </noscript>
+      </Head>
+      <body>
         <Main />
         <NextScript />
-      </body></>
+      </body>
     </Html>
   );
 }
