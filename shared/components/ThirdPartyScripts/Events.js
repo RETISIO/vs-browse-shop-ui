c6071309df5183bb76ffa9c91c38b9ee11b62be5
Harish <kjharish@aienterprise.com>
2023-04-20 16:43:28 +0530

    build issue

--- shared/components/ThirdPartyScripts/Events.js
+++ shared/components/ThirdPartyScripts/Events.js
@@ -1,11 +1,13 @@
 /* eslint-disable import/prefer-default-export */
 import { viewItem } from './gtag';
+import { RviewItem } from './RetisioEvents';
 
 const visitPDP = (itemData) => {
   viewItem(itemData);
   if(window.lstImplement) {
     window.lstImplement.browsedProduct(itemData);
   }
+  RviewItem(itemData);
 };
 
 export {
