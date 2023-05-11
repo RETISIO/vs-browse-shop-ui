812a78e3f99b89126dcb890afa3ceaf976cb823e
Harish <kjharish@aienterprise.com>
2023-05-11 13:21:59 +0530

    snb events

--- shared/components/ThirdPartyScripts/RetisioSDK.js
+++ shared/components/ThirdPartyScripts/RetisioSDK.js
@@ -1,11 +1,14 @@
+/* eslint-disable react/jsx-closing-bracket-location */
 /* eslint-disable @next/next/inline-script-id */
 import React from 'react';
 import Script from 'next/script';
 
 export default function RetisioSDK() {
   return (
-    <Script src="https://storage.googleapis.com/retisio-dm-web-sdk/retisio-sdk.js">
-      {`retisioSDK.init(${process.env.NEXT_PUBLIC_RETISIO_ID});`}
+    <Script src="https://storage.googleapis.com/retisio-dm-web-sdk/retisio-sdk.js" 
+    onLoad={() => {
+      retisioSDK.init(process.env.NEXT_PUBLIC_RETISIO_ID);
+    }}>
     </Script>
   );
 }
