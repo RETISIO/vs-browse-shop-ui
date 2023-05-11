1049f302b19dd22165ce25dfabda92673d45e892
Hansraj <hmacha@AIEnterprise.com>
2023-05-11 17:44:02 +0530

    AB-1182 implemented

--- shared/components/ImageCarousel/index.js
+++ shared/components/ImageCarousel/index.js
@@ -1,124 +1,155 @@
-import React, { useState, useEffect } from 'react';
-import Slider from 'react-slick';
-import Carousel from 'react-bootstrap/Carousel';
-import Modal from 'react-bootstrap/Modal';
-import NextImage from '../template/components/nextImage';
-import USDABadge from '../../../public/static/assets/usda-badge.png';
-import SaleBadge from '../../../public/static/assets/sale-badge.png';
-import MagnifyImage from '../template/components/magnifyImage';
+import React, { useState, useEffect } from 'react'
+import Slider from 'react-slick'
+import Carousel from 'react-bootstrap/Carousel'
+import Modal from 'react-bootstrap/Modal'
+import NextImage from '../template/components/nextImage'
+import USDABadge from '../../../public/static/assets/usda-badge.png'
+import SaleBadge from '../../../public/static/assets/sale-badge.png'
+import MagnifyImage from '../template/components/magnifyImage'
 
-function ImageCarousel({ data, additionalDetails, onSale }) {
-  const damPath = process.env.NEXT_PUBLIC_IMAGEPATH;
-  const defaultImg = data?.media ? [data?.media] : [];
-  const alternateImg = data?.alternateImages ? data.alternateImages : [];
-  const [selected, setSelected] = useState(defaultImg[0]);
-  const imgData = [...defaultImg, ...alternateImg];
-  const [show, setShow] = useState(false);
-  const [openImg, setOpenImg] = useState();
-  const [width, setWidth] = useState(992);
+function ImageCarousel ({ data, additionalDetails, onSale }) {
+  const damPath = process.env.NEXT_PUBLIC_IMAGEPATH
+  const defaultImg = data?.media ? [data?.media] : []
+  const alternateImg = data?.alternateImages ? data.alternateImages : []
+  const [selected, setSelected] = useState(defaultImg[0])
+  const imgData = [...defaultImg, ...alternateImg]
+  const [show, setShow] = useState(false)
+  const [openImg, setOpenImg] = useState()
+  const [width, setWidth] = useState(992)
 
   const settings = {
     slidesToShow: 3,
     slidesToScroll: 1,
     vertical: true,
     verticalSwiping: true,
-    infinite: false,
-  };
+    infinite: false
+  }
 
-  const handleShow = (img) => {
-    setOpenImg(img);
-    setShow(true);
-  };
+  const handleShow = img => {
+    setOpenImg(img)
+    setShow(true)
+  }
   useEffect(() => {
-    const dImg = data?.media ? [data?.media] : [];
-    setSelected(dImg[0]);
-  }, [data]);
+    const dImg = data?.media ? [data?.media] : []
+    setSelected(dImg[0])
+  }, [data])
 
   useEffect(() => {
     window.addEventListener('resize', () => {
-      setWidth(window.innerWidth);
-    });
-  }, []);
+      setWidth(window.innerWidth)
+    })
+  }, [])
+
+  const displayAlternateImages = (img, index) => {
+    let srcImg = img.thumbnailImg
+    let idImg = img.smallImg
+    if (index > 0) {
+      if (img?.thumbnailImg) {
+        srcImg = img.thumbnailImg
+        idImg = img.smallImg
+      } else {
+        srcImg = img.largeImg
+        idImg = img.largeImg
+      }
+    }
+    return (
+      <NextImage
+        alt={img.altText}
+        src={`${damPath}${srcImg}`}
+        height='80'
+        width='80'
+        id={`${idImg}-${index}`}
+        className='thmbnl'
+      />
+    )
+  }
 
   return (
     <>
-      <Modal dialogAs="div" dialogClassName="mv-popup" show={show} fullscreen={true} onHide={() => setShow(false)}>
+      <Modal
+        dialogAs='div'
+        dialogClassName='mv-popup'
+        show={show}
+        fullscreen={true}
+        onHide={() => setShow(false)}
+      >
         <Modal.Header closeButton></Modal.Header>
         <Modal.Body>
           <NextImage
             alt={openImg?.altText}
             src={`${damPath}${openImg?.largeImg}`}
             fill
-            className="thmbnl imgpos"
+            className='thmbnl imgpos'
           />
         </Modal.Body>
       </Modal>
-      <div className="image-carousel">
-        <div className="thumbnail-section">
+      <div className='image-carousel'>
+        <div className='thumbnail-section'>
           {imgData.length > 1 && (
             <Slider {...settings}>
               {imgData?.map((img, index) => (
                 <div
                   className={`${
-                  imgData.length <= 3 ? 'thumbnail-np' : 'thumbnail-image'
-                } ${img?.smallImg === selected?.smallImg ? 'selectedImg' : ''}`}
+                    imgData.length <= 3 ? 'thumbnail-np' : 'thumbnail-image'
+                  } ${
+                    img?.smallImg === selected?.smallImg ? 'selectedImg' : ''
+                  }`}
                   onClick={() => setSelected(img)}
                   key={`${img.smallImg}_${index}`}
                 >
-                  <NextImage
+                  {/* <NextImage
                     alt={img.altText}
                     src={`${damPath}${img?.thumbnailImg}`}
-                    height="80"
-                    width="80"
+                    height='80'
+                    width='80'
                     id={`${img.smallImg}-${index}`}
-                    className="thmbnl"
-                  />
+                    className='thmbnl'
+                  /> */}
+                  {displayAlternateImages(img, index)}
                 </div>
               ))}
             </Slider>
           )}
         </div>
         {selected && (
-          <div className="picture-section">
+          <div className='picture-section'>
             {additionalDetails?.productCategory === 'USDA PRIME' && (
               <NextImage
-                alt="USDA Prime"
+                alt='USDA Prime'
                 src={USDABadge}
                 height={45}
                 width={45}
-                id="usda-logo"
-                className="usda-prime-logo"
+                id='usda-logo'
+                className='usda-prime-logo'
               />
             )}
             {onSale && (
               <NextImage
-                alt="Sale"
+                alt='Sale'
                 src={SaleBadge}
                 height={45}
                 width={45}
-                id="sale-logo"
-                className="sale-logo"
+                id='sale-logo'
+                className='sale-logo'
               />
             )}
-            {width > 991
-              ? (
-                <MagnifyImage
-                  img={`${damPath}${selected?.largeImg}`}
-                  alt={selected?.altText}
-                  offset={{ vertical: 0, horizontal: 10 }}
-                />
-              )
-              : (
-                <MagnifyImage
-                  img={`${damPath}${selected?.largeImg}`}
-                  alt={selected?.altText}
-                  zoomPosition="original"
-                />
-              )}
+            {width > 991 ? (
+              <MagnifyImage
+                img={`${damPath}${selected?.largeImg}`}
+                alt={selected?.altText}
+                offset={{ vertical: 0, horizontal: 10 }}
+              />
+            ) : (
+              <MagnifyImage
+                img={`${damPath}${selected?.largeImg}`}
+                alt={selected?.altText}
+                zoomPosition='original'
+              />
+            )}
           </div>
         )}
       </div>
-      <div className="image-carousel-mv">
+      <div className='image-carousel-mv'>
         <Carousel interval={null} fade>
           {imgData?.map((img, index) => (
             <Carousel.Item
@@ -127,22 +158,22 @@ function ImageCarousel({ data, additionalDetails, onSale }) {
             >
               {/* {onSale && ( */}
               <NextImage
-                alt="Sale"
+                alt='Sale'
                 src={SaleBadge}
                 height={45}
                 width={45}
-                id="sale-logo"
-                className="sale-logo"
+                id='sale-logo'
+                className='sale-logo'
               />
               {/* )} */}
               {additionalDetails?.productCategory === 'USDA PRIME' && (
                 <NextImage
-                  alt="USDA Prime"
+                  alt='USDA Prime'
                   src={USDABadge}
                   height={45}
                   width={45}
-                  id="usda-logo"
-                  className="usda-prime-logo"
+                  id='usda-logo'
+                  className='usda-prime-logo'
                 />
               )}
               <NextImage
@@ -152,14 +183,14 @@ function ImageCarousel({ data, additionalDetails, onSale }) {
                 // width={400}
                 fill
                 id={`${img?.smallImg}-${index}`}
-                className="d-block w-100 imgpos"
+                className='d-block w-100 imgpos'
               />
             </Carousel.Item>
           ))}
         </Carousel>
       </div>
     </>
-  );
+  )
 }
 
-export default ImageCarousel;
+export default ImageCarousel
