6c5f0d72c89a3d07966c9a7e68c27496d71343ee
dattaprasad <dbhangle@aienterprise.com>
2023-05-16 22:26:46 +0530

    AB-1203 fix

--- shared/components/componentMap.js
+++ shared/components/componentMap.js
@@ -21,6 +21,14 @@ import ReviewSection from './reviewsection'
 import ShippingInfoAccordion from './productaccordions/ShippingInfoAccordion'
 import CookingInstructions from './productaccordions/CookingInstructions'
 import ProductRecommondation from './Sliders/ProductRecommondation'
+import loginIcon from '../../public/headerImg/mobile/account/acc-loginIcon.svg';
+import registerIcon from '../../public/headerImg/mobile/account/acc-registerIcon.svg';
+import dashboardIcon from '../../public/headerImg/mobile/account/acc-dashboardIcon.svg';
+import accountInfoIcon from '../../public/headerImg/mobile/account/acc-accountInfoIcon.svg'
+import addressBookIcon from '../../public/headerImg/mobile/account/acc-addressBookIcon.svg'
+import ordersIcon from '../../public/headerImg/mobile/account/acc-ordersIcon.svg'
+import wishlistIcon from '../../public/headerImg/mobile/account/acc-wishlistIcon.svg'
+import logOutIcon from '../../public/headerImg/mobile/account/acc-logOutIcon.svg'
 
 const ComponentMap = {
   nextImage: NextImage,
@@ -42,5 +50,14 @@ const ComponentMap = {
   'pdpcookinginstructions:custom': CookingInstructions,
   'pdpyotpoplaceholder:custom': ReviewSection,
   'productRecommendations:custom': ProductRecommondation,
+  loginIcon,
+  registerIcon,
+  dashboardIcon,
+  accountInfoIcon,
+  addressBookIcon,
+  ordersIcon,
+  wishlistIcon,
+  logOutIcon,
+
 }
 export default ComponentMap
