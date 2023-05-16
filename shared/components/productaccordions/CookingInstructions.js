f64d0f32d10e4031b895ea1b0c1270680ac11789
Harish <kjharish@aienterprise.com>
2023-05-16 11:38:34 +0530

    cooking guide

--- shared/components/productaccordions/CookingInstructions.js
+++ shared/components/productaccordions/CookingInstructions.js
@@ -57,85 +57,87 @@ function CookingInstructions (props) {
   }
 
   return (
-    <div
-      className={`accordions-section ${
-        pdpDetails?.additionalDetails?.cookingGuideProductIds &&
-        pdpDetails?.additionalDetails?.cookingGuideProductIds?.length > 0
-          ? ''
-          : 'hide'
-      }`}
-    >
-      <div className='container'>
-        <Accordion flush>
-          <Accordion.Item eventKey='0'>
-            <Accordion.Header>
-              <div className='panel panel-default panel-large'>
-                <div className='panel-heading'>
-                  <div className='panel-title'>
-                    <a
-                      id='cookingInfoSelector'
-                      className='collapse'
-                      role='button'
-                      data-toggle='collapse'
-                      href='#cookingInfoPanel'
-                    >
-                      Cooking Instructions
-                      <i className='icon fas fa-chevron-down'></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </Accordion.Header>
-            <Accordion.Body>
-              <div className='panel-body'>
-                <div className='cooking-instruction-button'>
-                  <button
-                    id='print-btn'
-                    className='printbtn btn-primary btn-inverse btn-print'
-                    onClick={() => handlePrint()}
-                  >
-                    <i className='icon icon-left fas fa-print'></i>
-                    &nbsp; Print
-                  </button>
-                  <div className='btn-group'>
-                    <a
-                      className='sharebtn btn-primary btn-inverse btn-share dropdown-toggle'
-                      href={`mailto:?subject=${data?.name}&body=Check out these cooking instructions from Allen Brothers: https://www.allenbrothers.com/article/${data?.name}/${contentId}`}
-                    >
-                      <i className='icon icon-left fas fa-share-square'></i>
-                      &nbsp; Share
-                    </a>
-                  </div>
-                </div>
-                <div className='cooking-instruction-accordion'>
-                  <div className='cooking-instruction-caption-accordion'>
-                    <div className='cooking-instruction-heading-accordion'>
-                      <h1>
-                        {'Cooking Instructions: '}
-                        <span className='text-slim'>{data?.name}</span>
-                      </h1>
-                      <p className='serving-size'>
-                        <strong>{'Serving Size: '}</strong>
-                        <span className='text-slim'>{data?.servingSize}</span>
-                      </p>
-                    </div>
-                    <br />
-                    <div className='cooking-description-accordion'>
-                      {description && (
-                        <HtmlContent
-                          contentItemObj={description}
-                          isNextJs={true}
-                        />
-                      )}
+    <>
+      {description && (<div
+        className={`accordions-section ${
+          pdpData?.productDetails?.productAssociations?.cooking_guides &&
+          pdpData?.productDetails?.productAssociations?.cooking_guides?.length > 0
+            ? ''
+            : 'hide'
+        }`}
+      >
+        <div className='container'>
+          <Accordion flush>
+            <Accordion.Item eventKey='0'>
+              <Accordion.Header>
+                <div className='panel panel-default panel-large'>
+                  <div className='panel-heading'>
+                    <div className='panel-title'>
+                      <a
+                        id='cookingInfoSelector'
+                        className='collapse'
+                        role='button'
+                        data-toggle='collapse'
+                        href='#cookingInfoPanel'
+                      >
+                        Cooking Instructions
+                        <i className='icon fas fa-chevron-down'></i>
+                      </a>
                     </div>
                   </div>
                 </div>
-              </div>
-            </Accordion.Body>
-          </Accordion.Item>
-        </Accordion>
-      </div>
-    </div>
+              </Accordion.Header>
+              <Accordion.Body>
+                <div className='panel-body'>
+                  <div className='cooking-instruction-button'>
+                    <button
+                      id='print-btn'
+                      className='printbtn btn-primary btn-inverse btn-print'
+                      onClick={() => handlePrint()}
+                    >
+                      <i className='icon icon-left fas fa-print'></i>
+                      &nbsp; Print
+                    </button>
+                    <div className='btn-group'>
+                      <a
+                        className='sharebtn btn-primary btn-inverse btn-share dropdown-toggle'
+                        href={`mailto:?subject=${data?.name}&body=Check out these cooking instructions from Allen Brothers: https://www.allenbrothers.com/article/${data?.name}/${contentId}`}
+                      >
+                        <i className='icon icon-left fas fa-share-square'></i>
+                        &nbsp; Share
+                      </a>
+                    </div>
+                  </div>
+                  <div className='cooking-instruction-accordion'>
+                    <div className='cooking-instruction-caption-accordion'>
+                      <div className='cooking-instruction-heading-accordion'>
+                        <h1>
+                          {'Cooking Instructions: '}
+                          <span className='text-slim'>{data?.name}</span>
+                        </h1>
+                        <p className='serving-size'>
+                          <strong>{'Serving Size: '}</strong>
+                          <span className='text-slim'>{data?.servingSize}</span>
+                        </p>
+                      </div>
+                      <br />
+                      <div className='cooking-description-accordion'>
+                        {description && (
+                          <HtmlContent
+                            contentItemObj={description}
+                            isNextJs={true}
+                          />
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        </div>
+      </div>)}
+    </>
   )
 }
 
