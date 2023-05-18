c604970ec9478f1296e0e2c432cc88c1c53b4048
dattaprasad <dbhangle@aienterprise.com>
2023-05-18 13:27:34 +0530

    Added the mobile view as state in user context

--- shared/context/appContext.js
+++ shared/context/appContext.js
@@ -1,27 +1,49 @@
 /* eslint-disable import/named */
 /* eslint-disable react/jsx-no-constructed-context-values */
-import { getCookie } from '@retisio/sf-api'
-import { createContext, useContext, useState, useEffect } from 'react'
-import globalState from './globalstate.json'
-import { requestContructor } from '../helpers/api'
+import { getCookie } from '@retisio/sf-api';
+import {
+  createContext, useContext, useState, useEffect,
+} from 'react';
+import globalState from './globalstate.json';
+import { requestContructor } from '../helpers/api';
 
-const AppContext = createContext()
+const AppContext = createContext();
 
-export function AppWrapper ({ children }) {
-  const [state, updateState] = useState(globalState, '', {}, false)
-  const [show, setShow] = useState(false)
-  const [isLogged, setisLogged] = useState(false)
-  const [openReset, updateOpenReset] = useState(false)
-  const [loader, setLoader] = useState(false)
-  const [noReload, setNoReload] = useState(false)
+export function AppWrapper({ children }) {
+  const [state, updateState] = useState(globalState, '', {}, false);
+  const [show, setShow] = useState(false);
+  const [isLogged, setisLogged] = useState(false);
+  const [openReset, updateOpenReset] = useState(false);
+  const [loader, setLoader] = useState(false);
+  const [noReload, setNoReload] = useState(false);
 
   useEffect(() => {
-    ;(async () => {
+    (async() => {
       if (!getCookie('x-anyms-id')) {
-        await requestContructor('getUUID')
+        await requestContructor('getUUID');
       }
-    })()
-  }, [])
+    })();
+  }, []);
+
+  /* Check for Mobile View */
+  const [isMobileView, setIsMobileView] = useState(false);
+  const checkForMobileView = () => {
+    const width = window?.innerWidth || document?.documentElement?.clientWidth;
+    if (width < 768 && !isMobileView) setIsMobileView(true);
+    else if (width >= 768 && isMobileView) setIsMobileView(false);
+  };
+  useEffect(() => {
+    if (window) {
+      window?.addEventListener('resize', checkForMobileView);
+      checkForMobileView();
+    }
+    return () => {
+      if (window) {
+        window.removeEventListener('resize', checkForMobileView);
+      }
+    };
+  }, []);
+  /* Check for Mobile View ends */
 
   return (
     <AppContext.Provider
@@ -37,14 +59,15 @@ export function AppWrapper ({ children }) {
         loader,
         setLoader,
         noReload,
-        setNoReload
+        setNoReload,
+        isMobileView,
       }}
     >
       {children}
     </AppContext.Provider>
-  )
+  );
 }
 
-export function useAppContext () {
-  return useContext(AppContext)
+export function useAppContext() {
+  return useContext(AppContext);
 }
