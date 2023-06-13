c8462a38cd692608d36f77cc276b38fb5b8180fd
Hansraj <hmacha@AIEnterprise.com>
2023-06-13 20:28:37 +0530

    ABUAT-188 fixed

--- shared/components/productaccordions/CookingDescription.js
+++ shared/components/productaccordions/CookingDescription.js
@@ -12,7 +12,9 @@ export default function CookingDescription ({ id, data, description }) {
       'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0'
     )
     WinPrint.document.open()
-    WinPrint.document.write("<html><head><title>Cooking Instructions</title></head><body>"+printContent+"</body></html>")
+    WinPrint.document.write(
+      `<html><head><title>Cooking Instructions</title></head><body>${printContent}</body></html>`
+    )
     WinPrint.document.close()
     WinPrint.focus()
     WinPrint.print()
@@ -21,22 +23,26 @@ export default function CookingDescription ({ id, data, description }) {
   return (
     <div className='panel-body cooking-body'>
       <div className='cooking-instruction-button'>
-        <button
-          id='print-btn'
-          className='printbtn btn-primary btn-inverse btn-print'
-          onClick={() => handlePrint()}
-        >
-          <i className='icon icon-left fas fa-print'></i>
-          &nbsp; Print
-        </button>
-        <div className='btn-group'>
-          <a
-            className='sharebtn btn-primary btn-inverse btn-share dropdown-toggle'
-            href={`mailto:?subject=${data?.name}&body=Check out these cooking instructions from Allen Brothers: https://www.allenbrothers.com/article/${data?.name}/${id}`}
+        <div className='cooking-instruction-printbtn'>
+          <button
+            id='print-btn'
+            className='printbtn btn-primary btn-inverse btn-print'
+            onClick={() => handlePrint()}
           >
-            <i className='icon icon-left fas fa-share-square'></i>
-            &nbsp; Share
-          </a>
+            <i className='icon icon-left fas fa-print'></i>
+            &nbsp; Print
+          </button>
+        </div>
+        <div className='cooking-instruction-sharebtn'>
+          <div className='btn-group'>
+            <a
+              className='sharebtn btn-primary btn-inverse btn-share dropdown-toggle'
+              href={`mailto:?subject=${data?.name}&body=Check out these cooking instructions from Allen Brothers: https://www.allenbrothers.com/article/${data?.name}/${id}`}
+            >
+              <i className='icon icon-left fas fa-share-square'></i>
+              &nbsp; Share
+            </a>
+          </div>
         </div>
       </div>
       <div className='cooking-instruction-accordion' id={id}>
