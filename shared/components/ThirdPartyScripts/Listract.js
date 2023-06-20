75ffc26a50e67e2a77f5f3315bf93b15900937d9
Harish <kjharish@aienterprise.com>
2023-06-20 19:53:29 +0530

    jquery

--- shared/components/ThirdPartyScripts/Listract.js
+++ shared/components/ThirdPartyScripts/Listract.js
@@ -4,10 +4,17 @@ import Script from 'next/script';
 import config from '../../helpers/getConfig';
 
 export default function Listract() {
+  useEffect(()=>{
+    const script = document.createElement("script");
+
+    script.src = `https://cdn.listrakbi.com/scripts/script/script.js?m=${config.LISKEY}&v=1`;
+    script.defer = true;
+
+    document.body.appendChild(script);
+  },[]);
   return (
     <>
       <Script src="/js/jquery.js"></Script>
-      <Script src={`https://cdn.listrakbi.com/scripts/script/script.js?m=${config.LISKEY}&v=1`}></Script>
     </>
   );
 }
