56e451776c86609b5ef0db5c87d84455bd8d6cba
Harish <kjharish@aienterprise.com>
2023-06-30 12:49:12 +0530

    favi icon issue

--- pages/_document.js
+++ pages/_document.js
@@ -21,7 +21,7 @@ export default function Document() {
             style={{display:"none",visibility:"hidden"}}
           >
           </iframe>
-
+          <link rel="icon" href="/favicon.ico" sizes="any" />
         </noscript>
       </Head>
       <body>
