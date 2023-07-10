1bb98a57bc1726b41cdba7748df876fc5aef5d33
Hansraj <hmacha@AIEnterprise.com>
2023-07-10 15:35:55 +0530

    AB-2131 implemented

--- shared/components/template/components/magnifyImage.js
+++ shared/components/template/components/magnifyImage.js
@@ -1,35 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import ReactImageZoom from 'react-image-zoom';
-import NoImage from '../../../../public/static/assets/no-image.png';
+import React, { useState, useEffect } from 'react'
+import ReactImageZoom from 'react-image-zoom'
+import NoImage from '../../../../public/static/assets/no-image.png'
 
-function MagnifyImage(props) {
-  const [data, setData] = useState(props);
+function MagnifyImage (props) {
+  const [data, setData] = useState(props)
 
-  const getImageOrFallback = (url, fallback) => new Promise((resolve, reject) => {
-    const img = new Image();
-    img.src = url;
-    img.onload = () => resolve(url);
-    img.onerror = () => {
-      reject(`image not found for url ${url}`);
-    };
-  }).catch(() => fallback?.src);
+  const getImageOrFallback = (url, fallback) =>
+    new Promise((resolve, reject) => {
+      const img = new Image()
+      img.src = url
+      img.onload = () => resolve(url)
+      img.onerror = () => {
+        reject(`image not found for url ${url}`)
+      }
+    }).catch(() => fallback?.src)
 
   useEffect(() => {
     // eslint-disable-next-line react/destructuring-assignment
-    getImageOrFallback(props?.img, NoImage)
-      .then((res) => {
-        setData({
-          ...props,
-          img: res,
-        });
-      });
-  }, [props]);
+    getImageOrFallback(props?.img, NoImage).then(res => {
+      setData({
+        ...props,
+        zoomPosition: 'original',
+        img: res
+      })
+    })
+  }, [props])
 
   return (
-    <div className="magnify-img">
+    <div className='magnify-img'>
       <ReactImageZoom {...data} />
     </div>
-  );
+  )
 }
 
-export default MagnifyImage;
+export default MagnifyImage
