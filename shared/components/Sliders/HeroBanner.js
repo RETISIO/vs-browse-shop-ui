136842edbffd6b0dcc0fb52031e7edccaf9a8339
skuamr-aie <skumar@aienterprise.com>
2023-08-18 16:30:48 +0530

    AB-2353,2338,2341

--- shared/components/Sliders/HeroBanner.js
+++ shared/components/Sliders/HeroBanner.js
@@ -4,49 +4,57 @@ import Slider from 'react-slick';
 import config from '../../helpers/getConfig';
 
 export default function HeroBanner(props) {
-    const configValues = props.configValue
+  const configValues = props.configValue
     ? JSON.parse(props.configValue)
     : { noOfRecords: 4 };
-    const content = props.content?JSON.parse(props.content):{};
-    var settings = {
-        className: "",
-        dots: configValues.heroBanner.navigationIcon == 'Arrows & Dots' || configValues.heroBanner.navigationIcon == 'Dots'?true:false,
-        arrows: configValues.heroBanner.navigationIcon == 'Arrows & Dots' || configValues.heroBanner.navigationIcon == 'Arrows'?true:false,
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        adaptiveHeight: true,
-        autoplay: configValues.heroBanner.autoPlay == "Enabled"?true:false, //
-        speed: 500,
-        autoplaySpeed: parseInt(configValues.heroBanner.autoPlaySpeed) || 1000,
-        cssEase: "linear"
-      };
+  const content = props.content ? JSON.parse(props.content) : {};
+  const settings = {
+    className: '',
+    dots: !!(configValues.heroBanner.navigationIcon == 'Arrows & Dots' || configValues.heroBanner.navigationIcon == 'Dots'),
+    arrows: !!(configValues.heroBanner.navigationIcon == 'Arrows & Dots' || configValues.heroBanner.navigationIcon == 'Arrows'),
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    autoplay: configValues.heroBanner.autoPlay == 'Enabled', //
+    speed: 500,
+    autoplaySpeed: parseInt(configValues.heroBanner.autoPlaySpeed) || 1000,
+    cssEase: 'linear',
+  };
     //   console.log('content',content);
   return (
     <Slider {...settings}>
-        { configValues.heroBanner && configValues.heroBanner.slides && configValues.heroBanner.slides.map((slide)=>{
-            return (<div  id="allenmainherobanner">
-            <div className="allenmainherobanner">
-                <div id="banner_main" style={{backgroundImage: `url(${config.IMGPATH + slide.relativeUrl+'/'+slide.name});`}}
-                    >
-                    <div id="overlay" className="row-full"></div>
-                    <div className="">
-                        {slide.informationPosition}
-                        <div className={slide.informationPosition=='right'? "text-right banner-content":"text-left banner-content"}>
-                            <h2 className="col-md-12 mobile-banner">
-                               {content?.heroBanner && content?.heroBanner['slides'][slide.id] &&  (<><span className="col-md-12 col-sm-8 banner-title">{content?.heroBanner['slides'][slide.id]?.primaryText}</span><span className="col-md-12 col-sm-8 banner-title">{content?.heroBanner['slides'][slide.id]?.secondaryText}</span></>)}
-                            </h2>
-                            <div className="banner-actions col-md-12 col-sm-8">
-                                {content?.heroBanner && content?.heroBanner['slides'][slide.id] && <a id="banner-button" href={content?.heroBanner['slides'][slide.id]?.buttonUrl} tabindex="-1"  className="btn btn-secondary btn-lg link-text" title={content?.heroBanner && content?.heroBanner['slides'][slide.id]?.buttonCaption}>
-                                {content?.heroBanner && content?.heroBanner['slides'][slide.id]?.buttonCaption}
-                                </a>}
-                            </div>
-                        </div>
-                    </div>
+      { configValues.heroBanner && configValues.heroBanner.slides && configValues.heroBanner.slides.map((slide) => (
+        <div id="allenmainherobanner">
+          <div className="allenmainherobanner mainHeroBanner">
+            <div
+              id="banner_main"
+              style={{ backgroundImage: `url(${`${config.IMGPATH + slide.relativeUrl}/${slide.name}`});` }}
+            >
+              <div id="overlay" className="row-full"></div>
+              <div>
+                <div className={slide.informationPosition == 'right' ? 'text-right banner-content' : 'text-left banner-content'}>
+                  <h2 className="mobile-banner">
+                    {content?.heroBanner && content?.heroBanner.slides[slide.id] && (
+                      <>
+                        <span className="col-md-12 col-sm-8 banner-title">{content?.heroBanner.slides[slide.id]?.primaryText}</span>
+                        <span className="col-md-12 col-sm-8 banner-title">{content?.heroBanner.slides[slide.id]?.secondaryText}</span>
+                      </>
+                    )}
+                  </h2>
+                  <div className="banner-actions col-md-12 col-sm-8">
+                    {content?.heroBanner && content?.heroBanner.slides[slide.id] && (
+                      <a id="banner-button" href={content?.heroBanner.slides[slide.id]?.buttonUrl} tabIndex="-1" className="btn btn-secondary btn-lg link-text" title={content?.heroBanner && content?.heroBanner.slides[slide.id]?.buttonCaption}>
+                        {content?.heroBanner && content?.heroBanner.slides[slide.id]?.buttonCaption}
+                      </a>
+                    )}
+                  </div>
                 </div>
+              </div>
             </div>
-        </div>)
-        })}
+          </div>
+        </div>
+      ))}
     </Slider>
-  )
+  );
 }
--- shared/components/customComponents/Featureproducts.js
+++ shared/components/customComponents/Featureproducts.js
@@ -52,44 +52,46 @@ export default function Featureproducts(props) {
   }
 
   const calcPrice = (product) => {
-    if(product && product.onSale && product?.productPrice?.minSalePrice && product?.productPrice?.minListPrice){
+    if(product && product.onSale && product?.productPrice?.minSalePrice && product?.productPrice?.minListPrice) {
       return product?.productPrice?.minListPrice < product?.productPrice?.minSalePrice ? product?.productPrice?.minListPrice.toFixed(2) : product?.productPrice?.minSalePrice.toFixed(2);
-    }else if(product?.productPrice && product?.productPrice.minListPrice){
+    }if(product?.productPrice && product?.productPrice.minListPrice) {
       return product?.productPrice?.minListPrice.toFixed(2);
-    }else{
-      return ''
-    };
+    }
+    return '';
   };
 
   const contentBlock = (conf) => {
     const data = productsData.featuredProducts[conf.productId];
     // console.log(conf,data);
     return (
-    <div className="col-sm-6" style={{ paddingLeft: '0px', paddingRight: '0px' }}>
-      <div className="promotion-banner">
-        <div className="promotion-banner-inner">
-          <h2 className="promotion-title">
-            {data.featuredProductName=="productName" && data.productDetails.displayName}
-            {data.featuredProductName=="customProductName" && data.customProductName}
-          </h2>
-          <p className="promotion-description">
-            {data.featuredProductDescription=="productDescription" && data.productDetails.description}
-            {data.featuredProductDescription=="customProductDescription" && data.customProductDescription}
-          </p>
-         {conf.displayPrice=='Yes' && (<div className="promotion-price">
-            <span className="promotion-price" style={{ fontWeight: '400', color: 'black' }}>{`${Object.keys(data.productDetails?.skus)?.length === 1 ? 'Starting At ' : 'Starting From '}`}</span>
-            <span className="promotion-price" style={{ color: 'black', fontWeight: '600' }}>${calcPrice(data.productDetails)}</span>
-          </div>)}
+      <div className="col-sm-6" style={{ paddingLeft: '0px', paddingRight: '0px' }}>
+        <div className="promotion-banner">
+          <div className="promotion-banner-inner">
+            <h2 className="promotion-title">
+              {data.featuredProductName == 'productName' && data.productDetails.displayName}
+              {data.featuredProductName == 'customProductName' && data.customProductName}
+            </h2>
+            <p className="promotion-description">
+              {data.featuredProductDescription == 'productDescription' && data.productDetails.description}
+              {data.featuredProductDescription == 'customProductDescription' && data.customProductDescription}
+            </p>
+            {conf.displayPrice == 'Yes' && (
+              <div className="promotion-price">
+                <span className="promotion-price" style={{ fontWeight: '400', color: 'black' }}>{`${Object.keys(data.productDetails?.skus)?.length === 1 ? 'Starting At ' : 'Starting From '}`}</span>
+                <span className="promotion-price" style={{ color: 'black', fontWeight: '600' }}>${calcPrice(data.productDetails)}</span>
+              </div>
+            )}
+          </div>
+          <a href={`/products/${data.productDetails.seoData && data.productDetails.seoData.slug ? data.productDetails.seoData.slug : data.productDetails.displayName.replace(/[\s~`!@#$%^&*()_+\-={[}\]|\\:;"'<,>.?/]+/g, '-').toLowerCase()}/${data.productDetails?.productId}`} className="btn btn-secondary btn-md">{data.buttonCaption}</a>
         </div>
-        <a href={`/products/${data.productDetails.seoData && data.productDetails.seoData.slug ? data.productDetails.seoData.slug : data.productDetails.displayName.replace(/[\s~`!@#$%^&*()_+\-={[}\]|\\:;"'<,>.?/]+/g, '-').toLowerCase()}/${data.productDetails?.productId}`} className="btn btn-secondary btn-md">{data.buttonCaption}</a>
       </div>
-    </div>
-  )};
+    );
+  };
   return (
     <div className="row homepagePromotionBanner">
       {loading && (
         configValues.featuredProducts.map((config) => (
-          <div className="row promotions-row row-gutter-sm-15">
+          <div className="row-gutter-sm-15">
             {config.imagePosition === 'Left' && (<>{ImageBlock(config)}{contentBlock(config)}</>)}
             {config.imagePosition === 'Right' && (<>{contentBlock(config)}{ImageBlock(config)}</>)}
           </div>
